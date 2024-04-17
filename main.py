c8b85c096c9e9d59f00aefb5e419852934e275e9
Art-mag <ya_tyoma1985@mail.ru>
2024-04-18 02:18:43 +0300

    new code3

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-print("конфликт")
-for i in range(9):
-    print("слово")
-a, b =9, 8
+import pygame
+import random
+pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/42648.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
